d172e0ea788b8d169579a38870977e0bb6549b10
tshusima-123 <adityakanodia2006@gmail.com>
2025-04-13 06:41:58 +0530

    board function more

--- tictactoe.py
+++ tictactoe.py
@@ -4,15 +4,45 @@ import  constants
 import numpy as np
 pygame.init()
 screen=pygame.display.set_mode((constants.WIDTH,constants.HEIGHT))
+
 pygame.display.set_caption("TIC TAC TOE AI")
 screen.fill(constants.BG_COLOR)
 class Board:
      def __init__(self):
           self.squares=np.zeros((3,3))
+          self.markedSq=0
+     def checkWin(self):
+          #vertical win
+          for i in range(3):
+               if(self.squares[0][i]==self.squares[1][i]==self.squares[2][i]!=0):
+                    return self.squares[0][i]
+          #horizontal win 
+          for i in range(3):
+               if(self.squares[i][0]==self.squares[i][1]==self.squares[i][2]!=0):
+                    return self.squares[i][1]
+          #diagonal wins
+          if(self.squares[0][0]==self.squares[1][1]==self.squares[2][2]!=0):
+               return self.squares[0][0]
+          if(self.squares[0][2]==self.squares[1][1]==self.squares[2][0]!=0):
+                return self.squares[1][1]
+          return 0#(nobody won till now(not a check for draw that is different will bw done thru is full function))
      def mark(self,rows,cols,player):
           self.squares[rows][cols]=player
+          self.markedSq+=1
      def empty(self,row,col):
           return self.squares[row][col]==0
+     def emptySquares(self):
+          empty=[]
+          for i in range(3):
+               for j in range(3):
+                    if(self.squares[i][j]==0):
+                         empty.push((i,j))
+          return empty
+               
+     def isFull(self):
+      return self.markedSq==9
+          
+     
    
 class Game:
      def __init__(self):
